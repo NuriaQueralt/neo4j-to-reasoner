176277b2fab1f7778cb85ac1d8a3d4ecb35a2b8e
Andrew Su <andrew.su@gmail.com>
2017-10-21 23:55:00 -0700

    example input and desired output

--- neo4j-to-reasoner.py
+++ neo4j-to-reasoner.py
@@ -7,10 +7,70 @@ from yaml import dump
 driver = GraphDatabase.driver("bolt://localhost:7690")
 session = driver.session()
 
-query = 'MATCH (source { name:"chlorcyclizine"}), (target { name: "Asthma"}), path=(source)-[*..3]-(target) WITH source, target, path, [r IN relationships(path) | type(r)] AS types RETURN path AS Path LIMIT 10;'
+query = 'MATCH (source { name:"chlorcyclizine"}), (target { name: "Asthma"}), path=(source)-[*..3]-(target) WITH source, target, path, [r IN relationships(path) | type(r)] AS types RETURN path AS Path LIMIT 1;'
 
 result = session.run(query)
 for record in result:
     print(record["Path"])
     a = record["Path"]
 
+a.nodes
+a.relationships
+
+# convert this structure
+'''
+>>> a.nodes
+(<Node id=59729 labels={'Chemicals & Drugs'} properties={'name': 'chlorcyclizine'}>, <Node id=47907 labels={'Chemicals & Drugs'} properties={'name': 'Atropine'}>, <Node id=49094 labels={'Chemicals & Drugs'} properties={'name': 'Potassium'}>, <Node id=153169 labels={'Disorders'} properties={'name': 'Asthma'}>)
+>>> a.relationships
+(<Relationship id=133156 start=47907 end=59729 type='ASSOCIATED_WITH_CDawCD' properties={'n_pmids': '1', 'pmids': '{35784}'}>, <Relationship id=2760 start=49094 end=47907 type='ASSOCIATED_WITH_CDawCD' properties={'n_pmids': '2', 'pmids': '{2804636, 7508351}'}>, <Relationship id=4507438 start=49094 end=153169 type='ASSOCIATED_WITH_CDawDO' properties={'n_pmids': '8', 'pmids': '{11882918, 8777958, 4008616, 6364906, 10639760, 13208020, 9602294, 2772863}'}>)
+'''
+
+# into this YAML
+'''
+--- 
+Path: 
+  Nodes: 
+    - 
+      Node: 
+        labels: "Chemicals & Drugs"
+        name: chlorcyclizine
+    - 
+      Node: 
+        labels: "Chemicals & Drugs"
+        name: Atropine
+    - 
+      Node: 
+        labels: "Chemicals & Drugs"
+        name: Potassium
+    - 
+      Node: 
+        labels: Disorders
+        name: Asthma
+  Relationships: 
+    - 
+      Edge: 
+        n_pmids: 1
+        pmids: 
+          - 35784
+        type: ASSOCIATED_WITH_CDawCD
+    - 
+      Edge: 
+        n_pmids: 2
+        pmids: 
+          - 2804636
+          - 7508351
+        type: ASSOCIATED_WITH_CDawCD
+    - 
+      Edge: 
+        n_pmids: 8
+        pmids: 
+          - 11882918
+          - 8777958
+          - 4008616
+          - 6364906
+          - 10639760
+          - 13208020
+          - 9602294
+          - 2772863
+        type: ASSOCIATED_WITH_CDawDO
+'''
