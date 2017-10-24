efe5e35bada66e7cf59db82ccc5e8edbc8161ef3
Andrew Su <andrew.su@gmail.com>
2017-10-24 10:22:30 -0700

    v1 of chlorcyclizine_asthma_10000.yaml

--- neo4j-to-reasoner.py
+++ neo4j-to-reasoner.py
@@ -3,19 +3,45 @@
 
 from neo4j.v1 import GraphDatabase, basic_auth
 from yaml import dump
+import re
+
+def parsePath( path ):
+    out = {}
+    out['Nodes'] = []
+    for node in path["Path"].nodes:
+        n = {}
+        n["name"] = node.properties['name']
+        n["label"] = list(node.labels)[0]
+        out['Nodes'].append(n)
+    out['Edges'] = []
+    for edge in path["Path"].relationships:
+        e = {}
+        e["n_pmids"] = list(map(int, edge.properties['n_pmids']))[0]
+        s_pmids = edge.properties['pmids']
+        a_pmids = re.sub("{|}| ","",s_pmids).split(",")
+        e["pmids"] = list(map(int, a_pmids))
+        e["type"] = edge.type
+        out['Edges'].append(e)
+    return out
+
 
 driver = GraphDatabase.driver("bolt://localhost:7690")
 session = driver.session()
 
-query = 'MATCH (source { name:"chlorcyclizine"}), (target { name: "Asthma"}), path=(source)-[*..3]-(target) WITH source, target, path, [r IN relationships(path) | type(r)] AS types RETURN path AS Path LIMIT 1;'
+query = 'MATCH (source { name:"chlorcyclizine"}), (target { name: "Asthma"}), path=(source)-[*..3]-(target) WITH source, target, path, [r IN relationships(path) | type(r)] AS types RETURN path AS Path LIMIT 10000;'
 
 result = session.run(query)
-for record in result:
-    print(record["Path"])
-    a = record["Path"]
 
-a.nodes
-a.relationships
+output = []
+for record in result:
+    z = parsePath(record)
+    output.append(z)
+
+
+#f = open('out.yaml','w')
+#f.write(dump(output, default_flow_style=False))
+#f.close()
+print(dump(output, default_flow_style=False))
 
 # convert this structure
 '''
