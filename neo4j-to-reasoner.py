91ac20f079f95a9d897911522ab846c736c47b0a
Andrew Su <andrew.su@gmail.com>
2017-11-01 10:24:43 -0700

    simplify query

--- neo4j-to-reasoner.py
+++ neo4j-to-reasoner.py
@@ -11,14 +11,14 @@ import sys
 def parsePath( path ):
     out = {}
     out['Nodes'] = []
-    for node in path["Path"].nodes:
+    for node in path["path"].nodes:
         n = {}
         n["name"] = node.properties['name']
         n["umlscui"] = node.properties['umlscui']
         n["label"] = list(node.labels)[0]
         out['Nodes'].append(n)
     out['Edges'] = []
-    for edge in path["Path"].relationships:
+    for edge in path["path"].relationships:
         e = {}
         e["n_pmids"] = list(map(int, edge.properties['n_pmids']))[0]
         s_pmids = edge.properties['pmids']
@@ -40,7 +40,7 @@ if(args.format not in ["json","yaml","json_text"]):
 driver = GraphDatabase.driver("bolt://localhost:7690")
 session = driver.session()
 
-query = 'MATCH (source { name:"chlorcyclizine"}), (target { name: "Asthma"}), path=(source)-[*..3]-(target) WITH source, target, path, [r IN relationships(path) | type(r)] AS types RETURN path AS Path LIMIT 10000;'
+query = 'MATCH path=(source)-[*..3]-(target) where source.name="chlorcyclizine" AND target.name="Asthma" return path limit 10;'
 
 result = session.run(query)
 
