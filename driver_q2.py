187fd276fe8f8116cdd7cf0bdc634f0c3d050a13
Andrew Su <andrew.su@gmail.com>
2017-11-09 12:46:02 -0800

    generate YAML files too

--- driver_q2.py
+++ driver_q2.py
@@ -16,8 +16,8 @@ for index, row in df.iterrows():
         continue
     print(str(row))
     a = row
-    output_filename = "output/"+str(index)+"_"+row.Drug.replace(" ","")+"_"+row.Condition.replace(" ","")+"_path3.txt"
-    cmd = "python3 neo4j-to-reasoner.py -s "+row.DrugCUI+"  -e "+row.ConditionCUI+"  -t cui -f json_text -p 3 > "+output_filename
+    output_filename = "output/"+str(index)+"_"+row.Drug.replace(" ","")+"_"+row.Condition.replace(" ","")+"_path3.yaml"
+    cmd = "python3 neo4j-to-reasoner.py -s "+row.DrugCUI+"  -e "+row.ConditionCUI+"  -t cui -f yaml -p 3 > "+output_filename
 
     if( Path(output_filename).exists() ):
         sys.stderr.write("Skipping " + output_filename +"\n")
