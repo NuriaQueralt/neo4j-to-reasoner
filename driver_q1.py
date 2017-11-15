f89ae7e13e4e924f1cffa0e39bae812475542e60
Andrew Su <andrew.su@gmail.com>
2017-11-15 00:46:10 -0800

    first pass at q1

---
+++ driver_q1.py
@@ -0,0 +1,28 @@
+#
+# driver_q1.py
+#
+# read in input files from data/q1-disease-list run neo4j-to-reasoner.py
+
+import pandas as pd
+import math
+import sys
+import subprocess
+from pathlib import Path
+
+df = pd.read_csv( 'results/q1-disease-list-cui.txt', header=0, sep="\t", keep_default_na=False)
+for index, row in df.iterrows():
+    if( row.DiseaseCUI == "" ):
+        sys.stderr.write("Skipping " + str(index)+"\n")
+        continue
+    print(str(row))
+    a = row
+    output_filename = "output/q1_"+str(index)+"_"+row.Disease.replace(" ","")+"_path3.yaml"
+    cmd = "python3 q1-neo4j-to-reasoner.py -s "+row.DiseaseCUI+"  -t cui -f yaml -p 3 > "+output_filename
+
+    if( Path(output_filename).exists() ):
+        sys.stderr.write("Skipping " + output_filename +"\n")
+        continue
+
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+    break
